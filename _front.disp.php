fb5606d3415db6b450882abbb6339eb6dbb75c08
Ariflaw <nurariflaw@gmail.com>
2016-08-25 20:49:38 +0700

    Update

--- _front.disp.php
+++ _front.disp.php
@@ -38,10 +38,17 @@ skin_container( NT_('Front Page Main Area'), array(
 	'featured_intro_after'    => $params['featured_intro_after'],
 	'block_start'             => $params['front_block_start'],
 	'block_end'               => $params['front_block_end'],
+	'block_body_start'        => '<div class="evo_widget_body">',
+	'block_body_end'       	  => '</div>',
 	'block_first_title_start' => $params['front_block_first_title_start'],
 	'block_first_title_end'   => $params['front_block_first_title_end'],
 	'block_title_start'       => $params['front_block_title_start'],
 	'block_title_end'         => $params['front_block_title_end'],
+	// Search
+	'search_input_before'  	  => '<div class="input-group">',
+	'search_input_after'   	  => '',
+	'search_submit_before' 	  => '<span class="input-group-btn">',
+	'search_submit_after'  	  => '</span></div>',
 ) );
 // --------------------- END OF "Front Page Main Area" CONTAINER -----------------------
 ?>
